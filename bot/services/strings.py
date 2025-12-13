f71fb3e90f7cadedc9b768dec163f2334f2592ac
Igor Mamaev <igor.mamaev1@gmail.com>
2025-12-13 13:40:01 +0300

    allow em-en dashes

--- bot/services/strings.py
+++ bot/services/strings.py
@@ -1,7 +1,7 @@
 import re
 from html import unescape
 
-SAFE_PATTERN = re.compile(r"^[\w\s\-\.,!()]+$", re.UNICODE)
+SAFE_PATTERN = re.compile(r"^[\w\s\-\.—–,!()]+$", re.UNICODE)
 
 
 def is_valid_title(title: str) -> bool:
